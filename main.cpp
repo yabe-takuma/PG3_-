66ccaefdbac102ec3e15e64741c50e4d656ee3b4
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-10 12:08:53 +0900

    PG3の1_02を修正した。

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include<stdio.h>
-int Recursive(int num) {
-	if (num >= 1072) {
-		return num;
+int Recursive(int num1,int num2,int num3) {
+	num2++;
+	num3=1072* num2;
+	if (num1 >= num3) {
+		return num1;
 	}
-	return (Recursive(num * 2 - 50 + num));
+	return (Recursive(num1 * 2 - 50 + num1,num2,num3));
 }
 
 int main() {
-	int num = 100; //再帰的な賃金
-
+	int recusivewage = 100; //再帰的な賃金
+	int i = 1; //1時間がたっていくにつれて１増加する変数
+	int generalwage = 0;  //一般賃金
 	int result; //計算結果
 
-	result = Recursive(num);
+	result = Recursive(recusivewage,i,generalwage);
 	printf("%d　再帰的な賃金\n", result);
 	return 0;
 }
