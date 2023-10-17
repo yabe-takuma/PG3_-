f3cde6933b58e30c5dc2102953e2d2eb09e46fd8
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-17 12:14:28 +0900

    PG3_1_04を取り組んでいるところ

--- main.cpp
+++ main.cpp
@@ -1,41 +1,12 @@
 #include<stdio.h>
-#include<Windows.h>
-#include<string.h>
-//macとかのUnix系OSの場合
-//#include<unistd.h>
-
-#define INPUT_STR "奇数"
-
-
-typedef void (*PFunc)(int*);
-//コールバック関数
-void DispResult(int *s) {
-	printf("%d待って実行されたよ\n", *s);
-}
-
-void setTimeout(PFunc p, int second) {
-	//コールバック関数を呼び出す
-	Sleep(second*1000);
-	//macとかUnix系OSの場合
-	//sleep(second);
-
-
-	p(&second);
-}
+#include <functional>
 
 int main() {
-	printf("start\n");
-	char name[8]="奇数";
-	char name2[8] = "奇数";
-	scanf_s("%s", name,8);
-	if (strcmp(name,"奇数") == 0)
-	{
-		printf("%s", name);
-	}
-	PFunc p;
-	p = DispResult;
-	setTimeout(p, 5);
 	
-	
+	std::function<int(int)> fx = [=](int i) {return i + 1; };
+	auto fx2 = [](int i) {return i + 1; };
+
+	printf("%d", fx(2));
+	printf("%d", fx2(2));
 	return 0;
 }
