c551f181a018ceafadfa4bff9e8638549e8276f7
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-24 10:56:10 +0900

    PG3の1_03が完成(仮)

--- main.cpp
+++ main.cpp
@@ -13,23 +13,25 @@ typedef void (*PFunc)(int*);
 
 int DiseRecyrsive(int n) {
 	
-	if (n == 0) {
+	if (n == 0||n==1) {
 		return 0;
 	}
-	else if (n == 1) {
-		return 1;
-	}
-	if (n >= 2||n<=-1) {
-		return DiseRecyrsive(n);
-	}
+
+	scanf_s("%d", &n);
+	return DiseRecyrsive(n);
+	
 }
 
 //コールバック関数
 void DispResult(int *s) {
 	int num = rand() % 6 + 1;
-	
-	printf("答えは%dでした。\n", num,*s);
-	
+	if (num == 2 || num == 4 || num == 6) {
+		printf("答えは%d丁(1)でした。\n", num, *s);
+	}
+	if (num == 1 || num == 3 || num == 5) {
+		printf("答えは%d半(0)でした。\n", num, *s);
+	}
+
 }
 
 void setTimeout(PFunc p, int second) {
@@ -47,10 +49,11 @@ int main() {
 
 
 
-	printf("start\n");
+	printf("0か1を入力してください\n");
 	time_t currentTime = time(nullptr);
 	srand((time_t)currentTime);
 	int name=0;
+	
 
 	scanf_s("%d", &name);
 	name= DiseRecyrsive(name);
