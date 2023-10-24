029c95445fa006a8f87377a1428c6e8db6e2f19c
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-24 11:23:21 +0900

    PG3の1_04をやっているところ

--- main.cpp
+++ main.cpp
@@ -8,18 +8,24 @@ int main() {
 	PFunc p;
 
 
-	std::function<int*(int*)> fx = [=](int *s) {
+	std::function<int(int)> fx = [=](int i) {
 		int num = rand() % 6 + 1;
 		if (num == 2 || num == 4 || num == 6) {
-			printf("答えは%d丁(1)でした。\n", num, *s);
+			printf("答えは%d丁(1)でした。\n", num, i);
 		}
 		if (num == 1 || num == 3 || num == 5) {
-			printf("答えは%d半(0)でした。\n", num, *s);
+			printf("答えは%d半(0)でした。\n", num, i);
 		}
-		
+		return i;
 		
 		};
-	auto fx2 = [](int i) {return i + 1; };
+	auto fx2 = [](int i,) {
+		Sleep(second * 1000);
+		
+
+
+		i(&second); 
+		};
 
 	printf("%d\n", fx(2));
 	printf("%d", fx2(2));
