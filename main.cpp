0048b95eb027e9d148fec5af7e84379d4bdf02b0
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-24 11:02:13 +0900

    PG3の1_04をやっているところ

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
 	std::function<int(int)> fx = [=](int i) {return i + 1; };
 	auto fx2 = [](int i) {return i + 1; };
 
-	printf("%d", fx(2));
+	printf("%d\n", fx(2));
 	printf("%d", fx2(2));
 	return 0;
 }
