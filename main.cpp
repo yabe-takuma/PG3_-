1e427729cd06772b9dce4e5159ef1125d562c00e
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-10-24 10:40:39 +0900

    PG3の1_03をやっているところ

--- main.cpp
+++ main.cpp
@@ -9,24 +9,31 @@
 
 typedef void (*PFunc)(int*);
 
-void Judgement(int num) {
-	if (num == 1 ||num== 3 || num==5)
-	{
-		printf("答えは%d半でした。", num);
+
+
+int DiseRecyrsive(int n) {
+	
+	if (n == 0) {
+		return 0;
 	}
-	else if (num == 2 || num==4 || num==6)
-	{
-		printf("答えは%d丁でした。", num);
+	else if (n == 1) {
+		return 1;
+	}
+	if (n >= 2||n<=-1) {
+		return DiseRecyrsive(n);
 	}
 }
 
 //コールバック関数
 void DispResult(int *s) {
-	//printf("%d待って実行されたよ\n", *s);
+	int num = rand() % 6 + 1;
+	
+	printf("答えは%dでした。\n", num,*s);
 	
 }
 
 void setTimeout(PFunc p, int second) {
+	
 	//コールバック関数を呼び出す
 	Sleep(second*1000);
 	//macとかUnix系OSの場合
@@ -37,17 +44,22 @@ void setTimeout(PFunc p, int second) {
 }
 
 int main() {
+
+
+
 	printf("start\n");
 	time_t currentTime = time(nullptr);
 	srand((time_t)currentTime);
-	char name[8];
-	int number = rand()  %6+1;
-	scanf_s("%s", name,8);
+	int name=0;
+
+	scanf_s("%d", &name);
+	name= DiseRecyrsive(name);
 	
 	PFunc p;
-	p=DispResult;
+	p = DispResult;
 	setTimeout(p, 3);
-	Judgement(number);
+	
+	
 	
 	return 0;
 }
