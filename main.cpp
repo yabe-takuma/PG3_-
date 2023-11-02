0eca50c82bd1c2d3668dc8b239c0842d9120b407
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-11-02 19:02:40 +0900

    PG3の2_01をやっている

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include<stdio.h>
 #include"MyClass.h"
 #include"Enemy.h"
+
+void Dinghan(PFunc p, int second) {
+	// プレイヤーの入力処理、奇数or偶数入力させる
+
+	Judgement();
+
+	//コールバック関数を呼び出す, 3秒まつ
+	Sleep(second * 1000);
+
+
+	//macとかUnix系OSの場合
+	//sleep(second);
+
+	// 結果表示
+	p();
+}
+
 int main() {
 
 	Enemy* enemy=new Enemy();
