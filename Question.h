ca599a02d2cc79d77f90ecd13485c0bf8a374170
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2023-11-14 10:26:20 +0900

    2_03をやっているところ

---
+++ Question.h
@@ -0,0 +1,17 @@
+#pragma once
+class Question
+{
+public:
+
+	enum 
+
+	void Generate();
+
+	void Attack();
+
+	void destruction();
+
+private:
+
+};
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include"Min.h"
+
 
 
 
@@ -8,24 +8,7 @@
 
 int main() {
 
-	Min<int,int>b1(10, 20);
-	Min<float,float>b2(13.5f, 15.5f);
-	Min<int,float>b3(30, 20.9f);
-	Min<int, double>b4(41, 40.5326f);
-	Min<float, double>b5(73.9f, 73.9874f);
-	Min<double, double>b6(10.4898f, 10.42109f);
-
-	printf("%d\n", b1.Minjugement());
-
-	printf("%f\n", b2.Minjugement());
-
-	printf("%f\n", b3.Minjugement());
-
-	printf("%lf\n", b4.Minjugement());
-
-	printf("%f\n", b5.Minjugement());
-
-	printf("%lf\n", b6.Minjugement());
+	
 
 	return 0;
 }
