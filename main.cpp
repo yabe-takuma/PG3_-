4e16be49e1fdfff49111664a4604ecde6d78d5a2
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-01-30 11:06:32 +0900

    PG3の06_02をやっているところ

--- main.cpp
+++ main.cpp
@@ -1,17 +1,5 @@
 #include<stdio.h>
 #include<thread>
-#include <functional>
-
-void Double(int num) {
-	num *= 2;
-	printf("num*2: %d\n", num);
-};
-
-void AddTwo(int num)
-{
-	num += 2;
-	printf("num+2: %d\n", num);
-};
 
 void thread1()
 {
