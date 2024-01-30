b988c7a861fc06c7256b9ff327b191132863954a
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-01-30 11:11:04 +0900

    PG3の06_02を資料通りに入力しているところ

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <functional>
-
+#include<thread>
+#include<stdio.h>
 void Double(int num) {
 	num *= 2;
 	printf("num*2: %d\n", num);
@@ -10,3 +11,25 @@ void AddTwo(int num)
 	num += 2;
 	printf("num+2: %d\n", num);
 };
+int main() {
+	int num = 3;
+	std::function<void(int)> Triple = [=](int num)
+		{
+			num *= 3;
+			printf("num*3: %d\n", num);
+		};
+
+	std::function<void(int)> AddThree = [=](int num)
+		{
+			num += 3;
+			printf("num+3: %d\n", num);
+		};
+
+	std::thread th1(Double, num);
+	std::thread th2(AddTwo, num);
+
+	th1.join();
+	th2.join();
+
+	return 0;
+}
